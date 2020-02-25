0579138b6455835bc0579a5cdb0837101913fe86
naamoonoo <naamoonoo.y@gmail.com>
2020-02-26 00:18:27 +0100

    #6-photoPartition for user and search list

---
+++ src/components/PhotoPartition/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+	container: {
+		// marginHorizontal: "auto",
+		// alignSelf: "center",
+		flexDirection: "row",
+		flexWrap: "wrap",
+		flexShrink: 1
+	},
+	image: {
+		width: 125,
+		height: 125,
+		// justifyContent: "center",
+		overflow: "hidden"
+	}
+});
+
+export default styles;
--- src/types/api.d.ts
+++ src/types/api.d.ts
@@ -7,17 +7,17 @@
 // ====================================================
 
 export interface CreateLike_CreateLike {
-	__typename: "CreateLikeResponse";
-	res: boolean;
-	error: string | null;
+  __typename: "CreateLikeResponse";
+  res: boolean;
+  error: string | null;
 }
 
 export interface CreateLike {
-	CreateLike: CreateLike_CreateLike;
+  CreateLike: CreateLike_CreateLike;
 }
 
 export interface CreateLikeVariables {
-	feedId: string;
+  feedId: string;
 }
 
 /* tslint:disable */
@@ -29,17 +29,17 @@ export interface CreateLikeVariables {
 // ====================================================
 
 export interface DeleteLike_DeleteLike {
-	__typename: "DeleteLikeResponse";
-	res: boolean;
-	error: string | null;
+  __typename: "DeleteLikeResponse";
+  res: boolean;
+  error: string | null;
 }
 
 export interface DeleteLike {
-	DeleteLike: DeleteLike_DeleteLike;
+  DeleteLike: DeleteLike_DeleteLike;
 }
 
 export interface DeleteLikeVariables {
-	feedId: string;
+  feedId: string;
 }
 
 /* tslint:disable */
@@ -51,40 +51,40 @@ export interface DeleteLikeVariables {
 // ====================================================
 
 export interface GetFeeds_GetFeeds_feeds_user {
-	__typename: "User";
-	id: string;
-	firstName: string;
-	profilePhoto: string;
+  __typename: "User";
+  id: string;
+  firstName: string;
+  profilePhoto: string;
 }
 
 export interface GetFeeds_GetFeeds_feeds_likes {
-	__typename: "Like";
-	userId: string | null;
+  __typename: "Like";
+  userId: string | null;
 }
 
 export interface GetFeeds_GetFeeds_feeds {
-	__typename: "Feed";
-	id: string;
-	photo: string;
-	text: string | null;
-	user: GetFeeds_GetFeeds_feeds_user | null;
-	likes: (GetFeeds_GetFeeds_feeds_likes | null)[] | null;
-	updateAt: string;
+  __typename: "Feed";
+  id: string;
+  photo: string;
+  text: string | null;
+  user: GetFeeds_GetFeeds_feeds_user | null;
+  likes: (GetFeeds_GetFeeds_feeds_likes | null)[] | null;
+  updateAt: string;
 }
 
 export interface GetFeeds_GetFeeds {
-	__typename: "GetFeedsResponse";
-	res: boolean;
-	error: string | null;
-	feeds: (GetFeeds_GetFeeds_feeds | null)[] | null;
+  __typename: "GetFeedsResponse";
+  res: boolean;
+  error: string | null;
+  feeds: (GetFeeds_GetFeeds_feeds | null)[] | null;
 }
 
 export interface GetFeeds {
-	GetFeeds: GetFeeds_GetFeeds;
+  GetFeeds: GetFeeds_GetFeeds;
 }
 
 export interface GetFeedsVariables {
-	page: number;
+  page: number;
 }
 
 /* tslint:disable */
@@ -96,22 +96,22 @@ export interface GetFeedsVariables {
 // ====================================================
 
 export interface SocialLogin_SocialLogin {
-	__typename: "SocialLoginResponse";
-	res: boolean;
-	error: string | null;
-	token: string | null;
+  __typename: "SocialLoginResponse";
+  res: boolean;
+  error: string | null;
+  token: string | null;
 }
 
 export interface SocialLogin {
-	SocialLogin: SocialLogin_SocialLogin;
+  SocialLogin: SocialLogin_SocialLogin;
 }
 
 export interface SocialLoginVariables {
-	googleId: string;
-	firstName: string;
-	lastName: string;
-	profilePhoto: string;
-	email: string;
+  googleId: string;
+  firstName: string;
+  lastName: string;
+  profilePhoto: string;
+  email: string;
 }
 
 /* tslint:disable */
@@ -123,37 +123,38 @@ export interface SocialLoginVariables {
 // ====================================================
 
 export interface GetUserById_GetUserById_user_feeds {
-	__typename: "Feed";
-	id: string;
-	photo: string;
+  __typename: "Feed";
+  id: string;
+  photo: string;
 }
 
 export interface GetUserById_GetUserById_user {
-	__typename: "User";
-	firstName: string;
-	profilePhoto: string;
-	feeds: (GetUserById_GetUserById_user_feeds | null)[] | null;
-	isEmailVerified: boolean | null;
-	isPhoneVerified: boolean | null;
-	email: string | null;
-	phone: string | null;
-	fbId: string | null;
-	googleId: string | null;
+  __typename: "User";
+  id: string;
+  firstName: string;
+  profilePhoto: string;
+  feeds: (GetUserById_GetUserById_user_feeds | null)[] | null;
+  isEmailVerified: boolean | null;
+  isPhoneVerified: boolean | null;
+  email: string | null;
+  phone: string | null;
+  fbId: string | null;
+  googleId: string | null;
 }
 
 export interface GetUserById_GetUserById {
-	__typename: "GetUserByIdResponse";
-	res: boolean;
-	error: string | null;
-	user: GetUserById_GetUserById_user | null;
+  __typename: "GetUserByIdResponse";
+  res: boolean;
+  error: string | null;
+  user: GetUserById_GetUserById_user | null;
 }
 
 export interface GetUserById {
-	GetUserById: GetUserById_GetUserById;
+  GetUserById: GetUserById_GetUserById;
 }
 
 export interface GetUserByIdVariables {
-	userId: string;
+  userId: string;
 }
 
 /* tslint:disable */
@@ -165,28 +166,28 @@ export interface GetUserByIdVariables {
 // ====================================================
 
 export interface GetCurrentUser_GetCurrentUser_user_likes {
-	__typename: "Like";
-	feedId: string | null;
+  __typename: "Like";
+  feedId: string | null;
 }
 
 export interface GetCurrentUser_GetCurrentUser_user {
-	__typename: "User";
-	id: string;
-	firstName: string;
-	profilePhoto: string;
-	likes: (GetCurrentUser_GetCurrentUser_user_likes | null)[] | null;
-	email: string | null;
-	phone: string | null;
+  __typename: "User";
+  id: string;
+  firstName: string;
+  profilePhoto: string;
+  likes: (GetCurrentUser_GetCurrentUser_user_likes | null)[] | null;
+  email: string | null;
+  phone: string | null;
 }
 
 export interface GetCurrentUser_GetCurrentUser {
-	__typename: "GetCurrentUserResponse";
-	res: boolean;
-	user: GetCurrentUser_GetCurrentUser_user | null;
+  __typename: "GetCurrentUserResponse";
+  res: boolean;
+  user: GetCurrentUser_GetCurrentUser_user | null;
 }
 
 export interface GetCurrentUser {
-	GetCurrentUser: GetCurrentUser_GetCurrentUser;
+  GetCurrentUser: GetCurrentUser_GetCurrentUser;
 }
 
 /* tslint:disable */
